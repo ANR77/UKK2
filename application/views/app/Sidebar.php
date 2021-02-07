f6d6277c249d6786a5e8031f6f4217cd549320ab
ANR77 <ramabilly3@gmail.com>
2021-02-07 18:47:14 +0700

    setting multi level sidebar, hapus item navbar sisa profile sama logout

--- application/views/app/Sidebar.php
+++ application/views/app/Sidebar.php
@@ -5,7 +5,7 @@
             <a class="navbar-brand w-100 mr-0" href="#" style="line-height: 25px;">
                 <div class="d-table m-auto">
                     <img id="main-logo" class="d-inline-block align-top mr-1" style="max-width: 25px;" src="<?= base_url('assets/template/') ?>images/shards-dashboards-logo.svg" alt="Shards Dashboard">
-                    <span class="d-none d-md-inline ml-1">Shards Dashboard</span>
+                    <span class="d-none d-md-inline ml-1">ASpp</span>
                 </div>
             </a>
             <a class="toggle-sidebar d-sm-inline d-md-none d-lg-none">
@@ -15,48 +15,97 @@
     </div>
     <div class="nav-wrapper">
         <ul class="nav flex-column">
-            <li class="nav-item">
-                <a class="nav-link active" href="index.html">
-                    <i class="material-icons">edit</i>
-                    <span>Blog Dashboard</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link " href="components-blog-posts.html">
-                    <i class="material-icons">vertical_split</i>
-                    <span>Blog Posts</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link " href="add-new-post.html">
-                    <i class="material-icons">note_add</i>
-                    <span>Add New Post</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link " href="form-components.html">
-                    <i class="material-icons">view_module</i>
-                    <span>Forms &amp; Components</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link " href="tables.html">
-                    <i class="material-icons">table_chart</i>
-                    <span>Tables</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link " href="user-profile-lite.html">
-                    <i class="material-icons">person</i>
-                    <span>User Profile</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link " href="errors.html">
-                    <i class="material-icons">error</i>
-                    <span>Errors</span>
-                </a>
-            </li>
+            <!-- ADMIN -->
+            <?php if ($this->session->level == 'admin') { ?>
+                <li class="nav-item">
+                    <a class="nav-link <?= ($title == 'Dashboard') ? "active" : " " ; ?>" href="components-blog-posts.html">
+                        <i class="fas fa-tachometer-alt"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+            <?php }?>
+            <?php if ($this->session->level == 'admin') { ?>
+                <li class="nav-item">
+                    <a class="nav-link <?= ($title == 'Transaksi') ? "active" : " " ; ?>" href="components-blog-posts.html">
+                        <i class="fas fa-cash-register"></i>
+                        <span>Transaksi</span>
+                    </a>
+                </li>
+            <?php }?>
+            <?php if ($this->session->level == 'admin') { ?>
+                <li class="nav-item">
+                    <a class="nav-link <?= ($title == 'Data Siswa') ? "active" : " " ; ?>" href="components-blog-posts.html">
+                        <i class="fas fa-user-graduate"></i>
+                        <span>Data Siswa</span>
+                    </a>
+                </li>
+            <?php }?>
+            <?php if ($this->session->level == 'admin') { ?>
+                <li class="nav-item">
+                    <a class="nav-link <?= ($title == 'Data Petugas') ? "active" : " " ; ?>" href="components-blog-posts.html">
+                        <i class="fas fa-user-tie"></i>
+                        <span>Data Petugas</span>
+                    </a>
+                </li>
+            <?php }?>
+            <?php if ($this->session->level == 'admin') { ?>
+                <li class="nav-item">
+                    <a class="nav-link <?= ($title == 'Data Kelas') ? "active" : " " ; ?>" href="components-blog-posts.html">
+                        <i class="fas fa-school"></i>
+                        <span>Data Kelas</span>
+                    </a>
+                </li>
+            <?php }?>
+            <?php if ($this->session->level == 'admin') { ?>
+                <li class="nav-item">
+                    <a class="nav-link <?= ($title == 'Data SPP') ? "active" : " " ; ?>" href="components-blog-posts.html">
+                        <i class="fas fa-file-invoice"></i>
+                        <span>Data SPP</span>
+                    </a>
+                </li>
+            <?php }?>
+            <?php if ($this->session->level == 'admin') { ?>
+                <li class="nav-item">
+                    <a class="nav-link <?= ($title == 'History') ? "active" : " " ; ?>" href="components-blog-posts.html">
+                        <i class="fas fa-history"></i>
+                        <span>History</span>
+                    </a>
+                </li>
+            <?php }?>
+            <!-- PETUGAS -->
+            <?php if ($this->session->level == 'petugas') { ?>
+                <li class="nav-item">
+                    <a class="nav-link <?= ($title == 'Dashboard') ? "active" : " " ; ?>" href="add-new-post.html">
+                        <i class="fas fa-cash-register"></i>
+                        <span>Transaksi</span>
+                    </a>
+                </li>
+            <?php }?>
+            <?php if ($this->session->level == 'petugas') { ?>
+                <li class="nav-item">
+                    <a class="nav-link <?= ($title == 'History') ? "active" : " " ; ?>" href="add-new-post.html">
+                        <i class="fas fa-history"></i>
+                        <span>History</span>
+                    </a>
+                </li>
+            <?php }?>
+            <!-- SISWA -->
+            <?php if ($this->session->level == 'siswa') {  ?>
+                <li class="nav-item <?= ($title == 'Profil') ? "active" : " " ; ?>">
+                    <a class="nav-link " href="form-components.html">
+                    <i class="fas fa-user"></i>
+                        <span>Profil</span>
+                    </a>
+                </li>
+            <?php }?>
+            <?php if ($this->session->level == 'siswa') {  ?>
+                <li class="nav-item <?= ($title == 'History') ? "active" : " " ; ?>">
+                    <a class="nav-link " href="form-components.html">
+                        <i class="fas fa-history"></i>
+                        <span>History</span>
+                    </a>
+                </li>
+            <?php }?>
         </ul>
     </div>
 </aside>
